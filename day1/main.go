8e415a76be3f982ed4326aaadecc302ed0e13f8a
Michal Kopczynski <26494112+michal-kopczynski@users.noreply.github.com>
2024-09-03 22:27:17 +0200

    Add solution for day 1 part 2. Handle overlapping digits.

--- day1/main.go
+++ day1/main.go
@@ -4,7 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+var digitsMap = map[string]string{
+	"zero":  "0o",
+	"one":   "1e",
+	"two":   "2o",
+	"three": "3e",
+	"four":  "4",
+	"five":  "5e",
+	"six":   "6",
+	"seven": "7n",
+	"eight": "8t",
+	"nine":  "9e",
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -16,10 +30,25 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	sum := 0
 	for scanner.Scan() {
-		sum += parseLine(scanner.Text())
+		sum += parseLine(decodeDigits(scanner.Text()))
 	}
 
 	fmt.Println(sum)
+}
+
+func decodeDigits(input string) string {
+	if len(input) == 0 {
+		return ""
+	}
+
+	for k, v := range digitsMap {
+		if strings.HasPrefix(input, k) {
+			input = strings.Replace(input, k, v, 1)
+			break
+		}
+	}
+
+	return input[0:1] + decodeDigits(input[1:])
 }
 
 func parseLine(line string) int {
@@ -30,7 +59,6 @@ func parseLine(line string) int {
 		num := int(runes[i] - '0')
 		if num >= 0 && num <= 9 {
 			firstDigit = num
-			fmt.Println(firstDigit)
 			break
 		}
 	}
--- day1/main_test.go
+++ day1/main_test.go
@@ -19,3 +19,25 @@ func TestParseLine(t *testing.T) {
 		}
 	}
 }
+
+func TestDecodeDigits(t *testing.T) {
+	type test struct {
+		input    string
+		expected string
+	}
+
+	tests := []test{
+		{input: "two1nine", expected: "2o19e"},
+		{input: "eighttwothree", expected: "8t2o3e"},
+		{input: "zoneeight234", expected: "z1e8t234"},
+		{input: "esevensevenseven", expected: "e7n7n7n"},
+		{input: "xtwone3four", expected: "x21e34"},
+		{input: "oneight", expected: "18t"},
+	}
+	for _, tc := range tests {
+		got := decodeDigits(tc.input)
+		if got != tc.expected {
+			t.Fatalf("expected: %s, got: %s", tc.expected, got)
+		}
+	}
+}
