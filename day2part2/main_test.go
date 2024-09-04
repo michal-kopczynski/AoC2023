e1ba13f254626de83b2cb7e8ffdd9973d0e19043
Michal Kopczynski <26494112+michal-kopczynski@users.noreply.github.com>
2024-09-04 13:25:38 +0200

    Add solution for day 2 part 2

---
+++ day2part2/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+func TestVerifyLine(t *testing.T) {
+	type test struct {
+		input    string
+		expected int
+	}
+
+	tests := []test{
+		{input: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", expected: 48},
+		{input: "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", expected: 12},
+		{input: "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", expected: 1560},
+		{input: "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", expected: 630},
+		{input: "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", expected: 36},
+	}
+
+	for _, tc := range tests {
+		got := verifyLine(tc.input)
+		if tc.expected != got {
+			t.Fatalf("expected: %d, got: %d", tc.expected, got)
+		}
+	}
+}
